e5ea98a8f893fc3db634f5b3037af4eeb3bfe1f1
Thien260102 <nguyenvanthien18209@gmail.com>
2023-10-01 21:54:41 +0700

    Creating all tables

--- Database.sql
+++ Database.sql
@@ -52,22 +52,149 @@ Create Table CUSTOMER
 ALTER TABLE CUSTOMER
 ADD CONSTRAINT PK_CUSTOMER PRIMARY KEY (ID, CitizenID);
 
+Drop table ROOM_TYPE;
+Create Table ROOM_TYPE
+(
+	ID int not null,
+	Name nvarchar(50),
+	HighestNumberPeople int,
+	Price money,
+)
+ALTER TABLE ROOM_TYPE
+ADD CONSTRAINT PK_ROOM_TYPE PRIMARY KEY (ID);
+
+Drop table ROOM;
+Create Table ROOM
+(
+	ID int not null,
+	Name nvarchar(50),
+	State nvarchar(10),
+	TypeID int,
+	Note nvarchar(max)
+)
+ALTER TABLE ROOM
+ADD CONSTRAINT PK_ROOM PRIMARY KEY (ID);
+
+Drop table RENTING;
+Create Table RENTING
+(
+	ID int not null,
+	CustomerID int,
+	UserName nvarchar(50),
+	RoomID int,
+	StartDate Date,
+	EndDate Date,
+	TotalDayStay int,
+)
+ALTER TABLE RENTING
+ADD CONSTRAINT PK_RENTING PRIMARY KEY (ID);
+
+Drop Table BOOKING
+Create Table BOOKING
+(
+	ID int not null,
+	CustomerID int,
+	UserName nvarchar(50),
+	RoomTypeID int,
+	CreateDate Date,
+	CheckinDate Date,
+	TotalDay int,
+	Total money,
+)
+ALTER TABLE BOOKING
+ADD CONSTRAINT PK_BOOKING PRIMARY KEY (ID);
+
+Drop table BILL
+Create Table BILL
+(
+	ID int not null,
+	BillDate Date,
+	UserName nvarchar(50),
+	Total money
+)
+ALTER TABLE BILL
+ADD CONSTRAINT PK_BILL PRIMARY KEY (ID);
+
+
+Drop table VOUCHER_TYPE
+Create Table VOUCHER_TYPE
+(
+	ID int not null,
+	Name nvarchar(50),
+	Ratio int,
+)
+ALTER TABLE VOUCHER_TYPE
+ADD CONSTRAINT PK_VOUCHER_TYPE PRIMARY KEY (ID);
+
+Drop table VOUCHER
+Create Table VOUCHER
+(
+	ID int not null,
+	CustomerID int,
+	ExpirationDate Date,
+	IsAvailable bit,
+	TypeID int
+)
+ALTER TABLE VOUCHER
+ADD CONSTRAINT PK_VOUCHER PRIMARY KEY (ID);
+
 
 --- Data to test ---
 
-
+--- ROLE ---
+Delete from ROLE;
 Insert into ROLE Values(1, 'Admin');
 Insert into ROLE Values(2, 'Manager');
 Insert into ROLE Values(3, 'Employee');
-
-
-Select * from ACCOUNT
-Insert into ACCOUNT Values('admin', '123', null, 1, 1, 0);
-delete from ACCOUNT
-
-Select * from EMPLOYEE where CitizenID = '123'
-delete from EMPLOYEE 
-insert into EMPLOYEE values(123, 'thien', 123, 0, '2022-12-12', '2022-12-12', 0)
-
 Select * from ROLE
 
+---------------
+
+--- ACCOUNT ---
+Delete from ACCOUNT;
+Select * from ACCOUNT
+Insert into ACCOUNT Values('admin', '123', null, 1, 1, 0);
+---------------
+
+--- EMPLOYEEE ----
+delete from EMPLOYEE 
+Select * from EMPLOYEE where CitizenID = '123'
+insert into EMPLOYEE values(123, 'thien', 123, 0, '2022-12-12', '2022-12-12', 0)
+------------------
+
+--- ROOM_TYPE ---
+Delete from ROOM_TYPE
+Insert into ROOM_TYPE Values(1, 'SMALL', 2, 200000);
+Insert into ROOM_TYPE Values(2, 'MEDIUM', 5, 400000);
+Insert into ROOM_TYPE Values(3, 'LARGE', 7, 500000);
+Insert into ROOM_TYPE Values(4, 'VIP', 2, 1000000);
+Select * from ROOM_TYPE
+
+-----------------
+
+--- ROOM ---
+Select * from ROOM
+------------------
+
+
+--- RENTING ---
+Select * from RENTING
+---------------
+
+--- BOOKING ---
+Select * from BOOKING
+---------------
+
+
+--- BILL ---
+Select * from BILL
+---------------
+
+
+--- VOUCHER_TYPE ---
+Select * from VOUCHER_TYPE
+---------------
+
+--- VOUCHER ---
+Select * from VOUCHER
+---------------
