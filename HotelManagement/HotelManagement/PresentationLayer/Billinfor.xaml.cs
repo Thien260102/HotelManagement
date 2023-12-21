4336de2a3ffd45ec8ff45e05a7df4180a175a982
VoTanTien <115779921+VoTanTien@users.noreply.github.com>
2023-12-21 15:51:43 +0700

    add Billinfor

---
+++ HotelManagement/HotelManagement/PresentationLayer/Billinfor.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HotelManagement.PresentationLayer
+{
+    /// <summary>
+    /// Interaction logic for Billinfor.xaml
+    /// </summary>
+    public partial class Billinfor : Window
+    {
+        public Billinfor()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_Save_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
